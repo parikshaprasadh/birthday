f8a62873503b5db7ee4bfa9af79c4be130b8cfb7
parikshaprasadh <parikshaprasadh@gmail.com>
2025-05-17 14:57:17 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function switchImage(){
+const img1 = document.getElementById("img1");
+const img2 = document.getElementById("img2");
+
+img1.classList.remove("visible");
+img2.classList.add("visible");
+}
