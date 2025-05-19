661ab62dbad36fc6270ad5fce329a7d20e6cb7b5
parikshaprasadh <parikshaprasadh@gmail.com>
2025-05-19 20:56:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 function switchImage(){
 const img1 = document.getElementById("img1");
 const img2 = document.getElementById("img2");
-
-img1.classList.remove("visible");
-img2.classList.add("visible");
-  
 const img3 = document.getElementById("img3");
 const img4 = document.getElementById("img4");
-
+  
+if (img1 && img2) {
+img1.classList.remove("visible");
+img2.classList.add("visible");
+}
+  
+if (img3 && img4) {
 img3.classList.remove("visible");
 img4.classList.add("visible");
+}
 }
