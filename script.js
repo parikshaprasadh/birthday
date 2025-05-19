1c4f7f461a544930a05b5cf7292bb5c3b9f62996
parikshaprasadh <parikshaprasadh@gmail.com>
2025-05-19 13:37:12 +0530

    after page3

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ const img2 = document.getElementById("img2");
 img1.classList.remove("visible");
 img2.classList.add("visible");
 }
+
+function switch(){
+const img3 = document.getElementById("img3");
+const img4 = document.getElementById("img4");
+
+img3.classList.remove("visible");
+img4.classList.add("visible");
+}
