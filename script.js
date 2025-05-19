4ffec61776f77f5228467a6a7ab5541c571348b3
parikshaprasadh <parikshaprasadh@gmail.com>
2025-05-19 20:15:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,9 +4,7 @@ const img2 = document.getElementById("img2");
 
 img1.classList.remove("visible");
 img2.classList.add("visible");
-}
-
-function open(){
+  
 const img3 = document.getElementById("img3");
 const img4 = document.getElementById("img4");
 
